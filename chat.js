224fcc14fe8f594f8e35bff141b329ace9fdbc17
Josip Igrec (Allypost) <allypost98@gmail.com>
2017-03-21 22:42:54 +0100

    Tiny little server

---
+++ chat.js
@@ -0,0 +1,29 @@
+let app  = require('express')();
+let http = require('http').Server(app);
+let io   = require('socket.io')(http);
+
+const PORT = 3000;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/templates/index.html');
+});
+
+io.on('connection', function (socket) {
+
+    socket.emit('user set', socket.id);
+
+    socket.broadcast.emit('user join', socket.id);
+
+    socket.on('chat message', function (msg) {
+        socket.broadcast.emit('chat message', { 'message': msg, 'user': socket.id });
+    });
+
+    socket.on('disconnect', function () {
+        socket.broadcast.emit('user leave', socket.id);
+    })
+
+});
+
+http.listen(PORT, function () {
+    console.log(`listening on *:${PORT}`);
+});
