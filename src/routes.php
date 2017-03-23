76b9d8a7205fcef7e570b04be0e436917fbe21d9
Josip Igrec (Allypost) <allypost98@gmail.com>
2017-03-23 10:39:28 +0100

    Moved online users into named route

--- src/routes.php
+++ src/routes.php
@@ -8,9 +8,13 @@ $app->get('/', Home::class . ':home')->setName('home');
 $app->post('/login', Home::class . ':auth')->setName('api:login');
 $app->get('/logout', Home::class . ':logout')->setName('api:logout');
 
-$app->get('/online', function (Request $request, Response $response) {
-    $o      = $this->get('o')->addResponse($response);
-    $online = array_values(json_decode(file_get_contents('http://localhost:3000/online'), TRUE));
+$app->group('/chat', function () {
 
-    return $o->say('chat users', $online);
+    $this->get('/online', function (Request $request, Response $response) {
+        $o      = $this->get('o')->addResponse($response);
+        $online = array_values(json_decode(file_get_contents('http://localhost:3000/online'), TRUE));
+
+        return $o->say('chat users', $online);
+    })->setName('api:chat:online');
+
 });
