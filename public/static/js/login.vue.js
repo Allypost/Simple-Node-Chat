01b1757640595efac372b9716d6d7ec78a748742
Josip Igrec (Allypost) <allypost98@gmail.com>
2017-03-23 04:22:12 +0100

    Login is now asynchronous

--- public/static/js/login.vue.js
+++ public/static/js/login.vue.js
@@ -1,3 +1,40 @@
 let loginVM = new Vue({
-    el: '#login'
+    el     : '#login',
+    data   : {
+        identifier: '',
+        password  : '',
+        loading   : false
+    },
+    methods: {
+        getURL() {
+            return this.$el.getAttribute('action');
+        },
+        send() {
+            let vm = this;
+
+            let data = {
+                identifier: vm.identifier,
+                password  : vm.password
+            };
+
+            vm.loading = true;
+
+            $.post(this.getURL(), data)
+             .done(function () {
+                 window.location.reload(true);
+             })
+             .fail(function (d) {
+                 let data = d.responseJSON;
+
+                 let errors = data.errors;
+
+                 for (let error in errors)
+                     if (errors.hasOwnProperty(error))
+                         Materialize.toast(errors[ error ], 5000, 'blue-grey darken-4 red-text');
+             })
+             .always(function () {
+                 vm.loading = false;
+             });
+        }
+    }
 });
