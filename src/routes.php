cea131901bbd891140386e0ecc0826196f35f917
Josip Igrec (Allypost) <allypost98@gmail.com>
2017-03-23 03:38:06 +0100

    Named home route

--- src/routes.php
+++ src/routes.php
@@ -2,5 +2,5 @@
 use App\Action\HomeAction as Home;
 
 // Routes
-$app->get('/', Home::class . ':home');
+$app->get('/', Home::class . ':home')->setName('home');
 $app->post('/login', Home::class . ':auth')->setName('api:login');
