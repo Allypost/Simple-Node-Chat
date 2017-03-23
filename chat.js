55c841281e317173bae4c877131c2f81d7a131c4
Josip Igrec (Allypost) <allypost98@gmail.com>
2017-03-23 04:42:32 +0100

    Just rearranged stuff

--- chat.js
+++ chat.js
@@ -1,17 +1,15 @@
 let io = require('socket.io')(3000, { transports: [ 'polling', 'websocket' ] });
 
 io.on('connection', function (socket) {
-
     socket.emit('user set', socket.id);
-
     socket.broadcast.emit('user join', socket.id);
 
-    socket.on('chat message', function (msg) {
-        socket.broadcast.emit('chat message', { 'message': msg, 'username': socket.id });
-    });
 
     socket.on('disconnect', function () {
         socket.broadcast.emit('user leave', socket.id);
     });
 
+    socket.on('chat message', function (msg) {
+        socket.broadcast.emit('chat message', { 'message': msg, 'username': socket.id });
+    });
 });
